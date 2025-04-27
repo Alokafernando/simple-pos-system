365eb1907c522daf0ff04e58426fcf36b8e58136
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-04-27 22:47:05 +0530

    initial jqueries for  items form

--- controller/main.js
+++ controller/main.js
@@ -1,14 +1,22 @@
 
     let dashboard = document.getElementById('dashboard');
-    let ordersPage = document.getElementById('orders');
+    let ordersForm = document.getElementById('orders');
+    let itemsForm = document.getElementById('items');
 
     $('#btn-home').on('click', function (e) {
     dashboard.style.display = 'block';
-    ordersPage.style.display = 'none';
+    ordersForm.style.display = 'none';
+    itemsForm.style.display = 'none';
 });
 
     $('#btn-orders').on('click', function (e) {
     dashboard.style.display = 'none';
-    ordersPage.style.display = 'block';
-});
+    ordersForm.style.display = 'block';
+    itemsForm.style.display = 'none';
+    });
 
+    $('#btn-items').on('click', function (){
+        dashboard.style.display = 'none';
+        ordersForm.style.display = 'none';
+        itemsForm.style.display = 'block';
+    });
