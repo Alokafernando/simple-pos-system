f41b06bd8acdfc05d32cb4314fa7a38e03796d15
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-04-27 22:55:01 +0530

    initial jqueries to customer manage form

--- controller/main.js
+++ controller/main.js
@@ -2,21 +2,32 @@
     let dashboard = document.getElementById('dashboard');
     let ordersForm = document.getElementById('orders');
     let itemsForm = document.getElementById('items');
+    let customerManageForm = document.getElementById('customers');
 
     $('#btn-home').on('click', function (e) {
     dashboard.style.display = 'block';
     ordersForm.style.display = 'none';
     itemsForm.style.display = 'none';
-});
+    customerManageForm.style.display = 'none';
+    });
 
     $('#btn-orders').on('click', function (e) {
     dashboard.style.display = 'none';
     ordersForm.style.display = 'block';
     itemsForm.style.display = 'none';
+    customerManageForm.style.display = 'none';
     });
 
     $('#btn-items').on('click', function (){
         dashboard.style.display = 'none';
         ordersForm.style.display = 'none';
         itemsForm.style.display = 'block';
+        customerManageForm.style.display = 'none';
+    });
+
+    $('#btn-customer-manage').on('click', function (){
+        dashboard.style.display = 'none';
+        ordersForm.style.display = 'none';
+        itemsForm.style.display = 'none';
+        customerManageForm.style.display = 'block';
     });
