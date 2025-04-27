9e600190d95182338768980cab55a64d85921d8d
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-04-27 23:17:54 +0530

    update project

--- controller/main.js
+++ controller/main.js
@@ -6,44 +6,60 @@ let customerManageForm = document.getElementById('customers');
 let customersAddingForm = document.getElementById('add-customers-form')
 
 $('#btn-home').on('click', function (e) {
+   showDashboard();
+});
+
+$('#btn-orders').on('click', function (e) {
+   showOrdersForm();
+});
+
+
+
+$('#btn-items').on('click', function (){
+   showItemsForm();
+});
+
+$('#btn-customer-manage').on('click', function (){
+    showCustomerManageForm();
+});
+
+$('#btn-add-customer-1, #btn-add-customer-2').on('click', function () {
+    showCustomerAddingForm();
+});
+
+
+const showDashboard = () => {
     dashboard.style.display = 'block';
     ordersForm.style.display = 'none';
     itemsForm.style.display = 'none';
     customerManageForm.style.display = 'none';
     customersAddingForm.style.display = 'none';
-});
+}
 
-$('#btn-orders').on('click', function (e) {
+const showOrdersForm = () => {
     dashboard.style.display = 'none';
     ordersForm.style.display = 'block';
     itemsForm.style.display = 'none';
     customerManageForm.style.display = 'none';
     customersAddingForm.style.display = 'none';
-});
+}
 
-$('#btn-items').on('click', function (){
+const showItemsForm = () => {
     dashboard.style.display = 'none';
     ordersForm.style.display = 'none';
     itemsForm.style.display = 'block';
     customerManageForm.style.display = 'none';
     customersAddingForm.style.display = 'none';
-});
+}
 
-$('#btn-customer-manage').on('click', function (){
+const showCustomerManageForm = () => {
     dashboard.style.display = 'none';
     ordersForm.style.display = 'none';
     itemsForm.style.display = 'none';
     customerManageForm.style.display = 'block';
     customersAddingForm.style.display = 'none';
-});
+}
 
-$('#btn-add-customer-1').on('click', function () {
-    showCustomerAddingForm();
-});
-
-$('#btn-add-customer-2').on('click', function () {
-    showCustomerAddingForm();
-});
 
 const showCustomerAddingForm = () => {
     dashboard.style.display = 'none';
