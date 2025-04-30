dabc90aa47ef7cbe7287e5c7378f3d8f036600cc
Aloka_fernando <140930974+Alokafernando@users.noreply.github.com>
2025-04-30 17:45:30 +0530

    initial order model

---
+++ model/OrderModel.js
@@ -0,0 +1,12 @@
+export class OrderModel{
+    constructor(custId, itemId, itemPrice, orderQuantity, orderId, orderDate, total, ) {
+        this.custId = custId;
+        this.itemId = itemId;
+        this.itemPrice = itemPrice;
+        this.orderQuantity = orderQuantity;
+        this.orderId = orderId;
+        this.orderDate = orderDate;
+        this.total = total;
+
+    }
+}
